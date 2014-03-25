e70c2d779405c36f2aab33c3d9dd3a50afd6764a
Wojciech Mioduszewski <wielkimiodas@interia.pl>
2014-03-25 10:04:31 +0100

    init project

---
+++ src/pl/poznan/put/TimeSeries/Main.java
@@ -0,0 +1,10 @@
+package pl.poznan.put.TimeSeries;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
