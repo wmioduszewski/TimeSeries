e897e91d09ebdd9cd8b299a6cfbcf95cfc0457e5
Wojciech Mioduszewski <wielkimiodas@interia.pl>
2014-04-15 06:14:16 +0200

    sax strings added

--- src/pl/poznan/put/TimeSeries/View/MainForm.java
+++ src/pl/poznan/put/TimeSeries/View/MainForm.java
@@ -1,8 +1,11 @@
 package pl.poznan.put.TimeSeries.View;
 
 import java.awt.Dimension;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Savepoint;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -93,22 +96,15 @@ public class MainForm {
 		// }
 		// }
 
-		String path = "C:/Users/Wojciech/Documents/studia/mgr/praca mgr/TimeSeries/output/charts/clean/";
-
-		for (Patient patient : patients) {
-			String d = patient.isSick() ? "D1" : "D0";
-			String name = "Clean " + patient.getId() +" " + d;
-			patient.setChartCaption(name);
-			//patient.setCharacteristics(Smoother.SmoothEWMACharacteristics(patient.getCharacteristics(), 0.3f));
-			JFreeChart jchart = LineChart.getPatientChart(Arrays.asList(patient));
-			try {
-				ChartUtilities.saveChartAsJPEG(new File(path + name +".jpg"),
-						jchart, 1600, 1000);
-				System.out.println("Narysowałem "+ name);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//		String folder = "C:/Users/Wojciech/Documents/studia/mgr/praca mgr/TimeSeries/output/charts/clean/";
+//		SaveChartsToFile(folder, patients);
+		
+		String path = "SaxStrings.txt";
+		try {
+			SaveSaxStringsToFile(path, patients);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		System.out.println("Koniec");
@@ -117,5 +113,36 @@ public class MainForm {
 		// window.pack();
 		// RefineryUtilities.centerFrameOnScreen(window);
 		// window.setVisible(true);
+	}
+	
+	private static void SaveSaxStringsToFile(String path, List<Patient> patients) throws IOException{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+
+		for (Patient patient : patients) {
+			String d = patient.isSick() ? "D1" : "D2";
+			writer.write(patient.getId() + " " + d + ":\t"+ patient.getSaxStrings().get(0).getContent() + System.getProperty("line.separator"));
+		}
+		
+		writer.flush();
+		writer.close();
+	}
+	
+	private static void SaveChartsToFile(String folder, List<Patient> patients)
+	{
+		for (Patient patient : patients) {
+			String d = patient.isSick() ? "D1" : "D0";
+			String name = "Clean " + patient.getId() +" " + d;
+			patient.setChartCaption(name);
+			//patient.setCharacteristics(Smoother.SmoothEWMACharacteristics(patient.getCharacteristics(), 0.3f));
+			JFreeChart jchart = LineChart.getPatientChart(Arrays.asList(patient));
+			try {
+				ChartUtilities.saveChartAsJPEG(new File(folder + name +".jpg"),
+						jchart, 1600, 1000);
+				System.out.println("Narysowałem "+ name);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
