59bc9337b8c56b92ef8eb86fa76b4ca12a8719d7
unknown <womi@WOMI-PRO.MobicaPL.local>
2015-03-20 08:26:56 +0100

    sax tests completed

--- src/test/java/pl/poznan/put/TimeSeries/DataImporters/DataImporterEamonnTest.java
+++ src/test/java/pl/poznan/put/TimeSeries/DataImporters/DataImporterEamonnTest.java
@@ -1,17 +1,16 @@
 package pl.poznan.put.TimeSeries.DataImporters;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import pl.poznan.put.TimeSeries.Model.EamonnRecord;
@@ -19,42 +18,92 @@ import pl.poznan.put.TimeSeries.Util.CommonConfig;
 
 public class DataImporterEamonnTest {
 
-	List<EamonnRecord> records = null ;
-	@Before
-	public void setUp() throws Exception {
-		
-		CommonConfig cfg = CommonConfig.getInstance();
-		cfg.setSaxAlphabeatSize(3);
-		cfg.setSaxOutputLength(7);
-		cfg = CommonConfig.getInstance();
-		
-		DataImporterEamonn importer = new DataImporterEamonn("testData");		
+	List<EamonnRecord> records = null;
+	private static String testSetName = "testData";
+
+	private List<EamonnRecord> importRecords() {
+		DataImporterEamonn importer = new DataImporterEamonn(testSetName);
 		try {
 			records = importer.importEamonnData();
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return records;
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		File f = new File(String.format("%s", testSetName));
+		f.mkdir();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		File f = new File(String.format("%s", testSetName));
+		f.delete();
+	}
+
+	private void setConfiguration(int alphabet, int output) {
+		CommonConfig cfg = CommonConfig.getInstance();
+		cfg.setSaxAlphabeatSize(alphabet);
+		cfg.setSaxOutputLength(output);
+	}
+
+	private void writeToFile(List<String> instances) {
+		File f = new File(
+				String.format("%s/%s_TRAIN", testSetName, testSetName));
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(f));
+
+			for (String string : instances) {
+				bw.write(string + System.getProperty("line.separator"));
+
+			}
+			bw.flush();
+			bw.close();
+
+			f = new File(String.format("%s/%s_TEST", testSetName, testSetName));
+			f.createNewFile();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-	}
-
-	@Test
-	public void saxWasApplied(){
-		assertTrue(records.size()>0);
-		for (EamonnRecord eamonnRecord : records) {
-			assertNotNull(eamonnRecord.getSaxString());			
 		}
 	}
-	
+
+	private void cleanupFiles() {
+		File f = new File(String.format("%s/%s_TEST", testSetName, testSetName));
+		f.delete();
+		f = new File(String.format("%s/%s_TRAIN", testSetName, testSetName));
+		f.delete();
+
+	}
+
+	private String saxTestBase(int alphabet, int output, String instance) {
+		setConfiguration(alphabet, output);
+		List<String> list = new ArrayList<String>();
+		list.add(instance);
+		writeToFile(list);
+		List<EamonnRecord> records = importRecords();
+		cleanupFiles();
+		return records.get(0).getSaxString();
+	}
+
 	@Test
-	public void saxWasCorrect(){
-		String sax0 = records.get(0).getSaxString();
-		String sax1 = records.get(1).getSaxString();
-		String sax2 = records.get(2).getSaxString();
-		
-		assertEquals(sax0, "bcbaaab");
-		assertEquals(sax1, "ccccbba");
-		assertEquals(sax1, "cccaaca");
-		//todo: dodać implementację i test na maksimum lokalne i globalne
+	public void sax1() {
+		String res = saxTestBase(3, 3, "0 1 2 3");
+		assertEquals(res, "abc");
+	}
+
+	@Test
+	public void sax2() {
+		String res = saxTestBase(3, 7, "0 0.5 0.5 2.2 2.3 3 0 1.5");
+		assertEquals(res, "aacccab");
+	}
+
+	@Test
+	public void sax3() {
+		String res = saxTestBase(3, 5, "0 1 1 2 0 0 1 2");
+		assertEquals(res, "bcaac");
 	}
 }
-
