60eef984885a316c016b4df73be79809f0d17dc3
Wojciech Mioduszewski <wielkimiodas@gmail.com>
2015-07-19 16:37:26 +0200

    comment out 'cutAttributes' method (unused currently)

--- src/main/java/pl/poznan/put/TimeSeries/DataExporters/SaxArffExporterBase.java
+++ src/main/java/pl/poznan/put/TimeSeries/DataExporters/SaxArffExporterBase.java
@@ -2,12 +2,10 @@ package pl.poznan.put.TimeSeries.DataExporters;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import pl.poznan.put.TimeSeries.Model.CalculatedRecord;
-import pl.poznan.put.TimeSeries.Util.CommonConfig;
 
 public abstract class SaxArffExporterBase extends ArffExporterBase {
 
@@ -23,16 +21,16 @@ public abstract class SaxArffExporterBase extends ArffExporterBase {
 	}
 
 	// TODO: probably should not be here, in this class
-	private void cutAttributes() {
-		float attributesToCutRatio = CommonConfig.getInstance()
-				.getAttributesToCutRatio();
-		int attributesToCut = (int) ((instances.numAttributes() - 1) * attributesToCutRatio);
-		Random rand = new Random();
-		for (int i = 0; i < attributesToCut; i++) {
-			int index = rand.nextInt(instances.numAttributes() - 1);
-			instances.deleteAttributeAt(index);
-		}
-	}
+//	private void cutAttributes() {
+//		float attributesToCutRatio = CommonConfig.getInstance()
+//				.getAttributesToCutRatio();
+//		int attributesToCut = (int) ((instances.numAttributes() - 1) * attributesToCutRatio);
+//		Random rand = new Random();
+//		for (int i = 0; i < attributesToCut; i++) {
+//			int index = rand.nextInt(instances.numAttributes() - 1);
+//			instances.deleteAttributeAt(index);
+//		}
+//	}
 
 	protected void getPeriodicDistincts() {
 		distincts = new ArrayList<List<String>>();
